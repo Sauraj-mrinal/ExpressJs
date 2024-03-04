81629bbd502d5086ed961268808a333ab97d10cb
msauraji <msauraji@gmail.com>
2024-03-04 22:58:04 +0530

    Add

--- Intro_with_express/index.js
+++ Intro_with_express/index.js
@@ -18,4 +18,60 @@ const app  = express(); // here function is calling and
 
 // so our task is to create our own server (small server)
 
+// TO create a server we need to use some 
+// http methods 
+//      1) get
+    //  2) post
+    //  3) Put 
+    //  4) delete
+
+
+
+
+// i want to create a route :::: go and see handwritten notes 
+
+app.get('/', (req, res) => {
+    res.send("home page ")
+})
+
+app.use(loginMiddleware());
+
+// we can create another route 
+
+app.get('/name', (req,res) =>{
+
+  res.send("my name is Mrinal Sauraj ");
+})
+
+
+app.get('/profilePage', (req,res) =>{
+
+  res.send("this is profilePage " );
+})
+
+app.get('/feedBack', (req,res) =>{
+
+  res.send("this is feedback page  ");
+})
+
+
+// to run this route we need a port ::
+// :: what is route and what is port :: go to my handwritten notes 
+
+port =8000;
+app.listen(port , ()=>{
+  console.log(" port is starting at 8000 :: Thank you :) ");
+})
+
+
+// middleware 
+
+// so here we will create a middleware function 
+function loginMiddleware(req, res , next){
+   console.log(" I am login middleware");
+   next();
+}
+
+
+
 
